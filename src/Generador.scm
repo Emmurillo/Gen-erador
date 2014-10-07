ec8d9e410c4b3d01a133bc8db73a6a67cdf73247
Emmurillo <emmanuel-ms@hotmail.com>
2014-10-07 11:10:30 -0600

    Operaciones binarias

--- src/Generador.scm
+++ src/Generador.scm
@@ -1,4 +1,17 @@
-(define operadores '(+ - / *))
+(define operadores '(+ - / * expt bin-and bin-or bin-xor))
+
+;OPERACIONES
+(define bin-and
+  (lambda (x y)
+    (bitwise-and (numerator x) (numerator y))))
+
+(define bin-or
+  (lambda (x y)
+    (bitwise-ior (numerator x) (numerator y))))
+
+(define bin-xor
+  (lambda (x y)
+    (bitwise-xor (numerator x) (numerator y))))
 
 ;ARCHIVOS
 
