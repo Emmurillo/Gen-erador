7b72bdb6e031420fcc671fef15f2b66207f6ce83
Emmurillo <emmanuel-ms@hotmail.com>
2014-10-07 10:48:39 -0600

    Obtener z

--- src/Generador.scm
+++ src/Generador.scm
@@ -1,4 +1,5 @@
 (define operadores '(+ - / *))
+
 ;ARCHIVOS
 
 ; Obtiene lista de numeros del archivo
@@ -25,12 +26,13 @@
 ;Remueve el n-ésimo elemento de la lista, donde el primer elemento es el índice 0
 ;Saca los z de la lista leída del archivo
 (define drop
-  (lambda (lista)
-    (drop-aux lista 0 2)))
+  (lambda (lista ind num)
+    (drop-aux lista ind num)))
 
 (define drop-aux
   (lambda (lista cont ind)
     (cond ((null? lista) lista)
+          ((equal? cont -1) (drop-aux (cdr lista) 0 ind))
           ((equal? cont ind) (drop-aux (cdr lista) 0 ind))
           (else
            (cons (car lista) (drop-aux (cdr lista) (+ 1 cont) ind))))))
@@ -43,6 +45,20 @@
           (else
            (append (list (cons (car lista) (list (cadr lista)))) (pack (cddr lista)))))))
 
+
+;Obtiene los X y Y empaquetados del archivo
+(define obtener-x-y
+  (lambda ()
+    (pack (drop (leer-archivo "file.txt") 0 2))))
+
+;Obtiene los X y Y empaquetados del archivo
+(define obtener-z
+  (lambda ()
+    (drop (drop (leer-archivo "file.txt") -1 2) 1 1)))
+
+
+
+
 ;OPERACIONES DE ARBOLES
 ;Obtiene un operador random
 (define operador-random 
@@ -52,7 +68,7 @@
 ; Genera un individuo aleatoriamente
 (define crea-individuo
   (lambda ()
-    (cond ((< (random) 0.49999) (crea-hoja))
+    (cond ((< (random) 0.6) (crea-hoja))
           (else
            (list (operador-random)
                  (crea-individuo)
@@ -62,8 +78,8 @@
 ;Determina el valor de una hoja
 (define crea-hoja
   (lambda ()
-    (cond ((< (random) 0.15) 'x)
-          ((< (random) 0.30) 'y)
+    (cond ((< (random) 0.15) 10)
+          ((< (random) 0.30) 20)
           (else
            (- 10 (random 20))))))
              
