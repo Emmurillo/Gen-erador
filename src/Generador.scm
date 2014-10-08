d40588b91c8c719d031c3c033b5acb852643f858
Emmurillo <emmanuel-ms@hotmail.com>
2014-10-08 13:49:02 -0600

    Evaluación con parámetros

--- src/Generador.scm
+++ src/Generador.scm
@@ -4,21 +4,28 @@
 
 ;OPERACIONES DE ARBOLES
 
-; Genera un individuo aleatoriamente
+; Genera un individuo de mínimo 1 de altura
 (define crea-individuo
+  (lambda ()
+    (list (operador-random)
+          (crea-individuo-aux)
+          (crea-individuo-aux))))
+
+; Genera un individuo aleatoriamente
+(define crea-individuo-aux
   (lambda ()
     (cond ((< (random) 0.6) (crea-hoja))
           (else
            (list (operador-random)
-                 (crea-individuo)
-                 (crea-individuo)
+                 (crea-individuo-aux)
+                 (crea-individuo-aux)
                  )))))
 
 ;Determina el valor de una hoja
 (define crea-hoja
   (lambda ()
-    (cond ((< (random) 0.15) 10)
-          ((< (random) 0.30) 20)
+    (cond ((< (random) 0.15) 'x)
+          ((< (random) 0.30) 'y)
           (else
            (- 10 (random 20))))))
 
@@ -28,5 +35,7 @@
     (cond ((= cantidad 0) '())
           (else (cons (crea-individuo) (crear-poblacion (- cantidad 1) ) )))))
 
-             
-
+(define eval-individuo
+  (lambda (individuo x y)
+    (display individuo) (newline)
+    ((eval (cons 'lambda(cons '(x y) (list individuo)))) x y)))
